5f2201fb3f06c9cd3b52ab1b91878152985e5137
9935358984yaar <79708844+9935358984yaar@users.noreply.github.com>
2021-12-29 10:13:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+img = "";
+status = "";
+object = "";
+
+function setup() {
+    canvas = createCanvas(380,380);
+    canvas.center();
+   video= createCapture(VIDEO);
+   video.size(380,380);
+   video.hide();
+
+}
+
+function start(){
+    document.getElementById("status").innerHTML="Getting on it right away...";
+    cocossd= ml5.objectDetector('cocossd',modelLoaded );
+    obj= document.getElementById("inputObject").value;
+    console.log(obj);
+}
+
+function preload() {
+    
+
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status=true;
+    cocossd.detect(video, gotResult);
+}
+
+function gotResult(error,result){
+    if(error){
+        console.log(error);
+    }
+}
